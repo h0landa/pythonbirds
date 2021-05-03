d461422a10ab726ddff4d99d4271c094a2402b8d
Arthur <arthurholanda410@gmail.com>
2021-05-03 15:25:48 -0300

    Criação do Exercicio: Carro

---
+++ oo/carro.py
@@ -0,0 +1,19 @@
+"""
+Você deve criar uma classe carro que vai possuir dois atributos compostos por outras duas classes:
+
+1) Motor
+2) Direção
+
+O Motor terá a responsabilidade de controlar a velocidade.
+Ele oferece os seguintes atributos:
+1) Atributo de dado velocidade
+2) Método acelerar, que deverá incrementar a velocidade de uma unidade
+3) Método frear que deverá decrementar a velocidade em duas unidades
+
+A Direção terá a responsabilidade de controlar a direção. Ela oferece os seguintes atributos:
+1) Valor de diração com valores posssiveis: Norte, Sul, Leste, Oeste.
+2) Método girar_a_direita
+  N
+O   L
+  S
+"""
